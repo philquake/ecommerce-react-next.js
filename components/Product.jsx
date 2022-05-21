e83bb9c327b4f19c4e6e74dbc914af6f389d8c56
philquake <phillipcole123@gmail.com>
2022-05-21 01:57:08 -0500

    made footer and product display

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link';
 
-export const Product = () => {
+import {urlFor} from '../lib/client';
+
+const Product = ({product:{image, name, clug, price}}) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={'/product/${slug.current}'}>
+        <div className="product-card">
+          <img src={urlFor(image && image[0])}
+          width={250}
+          height={250}
+          className = "product-image" />
+
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
